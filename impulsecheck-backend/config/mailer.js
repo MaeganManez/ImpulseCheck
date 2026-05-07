b849d3660681df2fce181c26d92aecfa3c7bfa93
Maegan Elaine S. Mañez <202410089@gordoncollege.edu.ph>
2026-05-08 04:57:11 +0800

    Switch to Gmail SMTP

--- impulsecheck-backend/config/mailer.js
+++ impulsecheck-backend/config/mailer.js
@@ -2,9 +2,7 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp-relay.brevo.com',
-  port: 587,
-  secure: true,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
